d91e2adc5c1b9eacd2e2f6c4e1feeaf3516a14eb
zhaokang <2568633106@qq.com>
2022-09-23 13:12:32 +0800

    hot-fix commit

--- src/main/java/com/User.java
+++ src/main/java/com/User.java
@@ -4,5 +4,6 @@ public class User {
     public static void main(String[] args) {
         System.out.println("1111111111111");
         System.out.println("hto-fix2");
+        System.out.println("new");
     }
 }
